ebf9bb538cc5d8bd63c04e175d286284615278ce
scad89 <scad200@gmail.com>
2022-04-25 22:20:41 +0300

    create app

---
+++ nested_comments_app/nested_comments_app/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from django.urls.conf import include
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    #    path('', include('main.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    #    path('api/v1/', include('main.urls'))
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
